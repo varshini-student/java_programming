1c038e48b3c47f59cf677a1e6f75a024793dfd1e
varshini-student <mvarshini1708@gmail.com>
2026-05-19 08:09:48 +0530

    printing a line or statemeent

---
+++ program4.java
@@ -0,0 +1,7 @@
+// printing a line or statement
+import java.util.Scanner;
+public class program4{
+    public static void main(String args[]){
+        System.out.printf("Happy Learning!");
+    }
+}
