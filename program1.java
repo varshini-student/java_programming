ff5afab33063ef6547b59cbc45ebe91c873ebdfd
varshini-student <mvarshini1708@gmail.com>
2026-05-19 07:59:46 +0530

    printing a single integer

---
+++ program1.java
@@ -0,0 +1,10 @@
+// printing a single integer
+import java.util.Scanner;
+public class program1 {
+    public static void main(String args[]){
+        Scanner scan =new Scanner(System.in);
+        short num=scan.nextShort();
+        System.out.printf("%d",num);
+    }
+}
+
