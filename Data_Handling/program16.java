a29228cc6d280324d8665fb60b8c9a3f7add9cd1
varshini-student <mvarshini1708@gmail.com>
2026-05-20 11:41:42 +0530

    floating-point with roundded values

---
+++ Data_Handling/program16.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class program16{
+    public static void main(String args []){
+        Scanner scan=new Scanner(System.in);
+        float num=scan.nextFloat();
+        System.out.printf("%.2f",num);
+    }
+}
