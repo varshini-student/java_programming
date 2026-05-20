aa5dd9fc03e3be124e760efff81f43ab683e4ca2
varshini-student <mvarshini1708@gmail.com>
2026-05-20 11:45:35 +0530

    concatenate two strings

---
+++ Data_Handling/program18.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class program18{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        String str1=scan.nextLine();
+        String str2=scan.nextLine();
+        System.out.printf(str1+str2);
+    }
+}
