77b185954a49eaace5fc274ae5de508226ca9da7
varshini-student <mvarshini1708@gmail.com>
2026-05-20 11:25:52 +0530

    spaces after the first number

---
+++ Data_Handling/program13.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class program13{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        long num1=scan.nextLong();
+        long num2=scan.nextLong();
+        System.out.printf("%d%17d", num1, num2);
+    }
+}
