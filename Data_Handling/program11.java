22e56db3b28fabd80d27745696959dd6a7134c80
varshini-student <mvarshini1708@gmail.com>
2026-05-20 11:17:05 +0530

    number representing an ascii value

---
+++ Data_Handling/program11.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class program11{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int num=scan.nextInt();
+        System.out.printf("%c",num);
+    }
+}
