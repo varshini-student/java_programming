930685d48d745e8a76d212f764222a33a2ebc8c7
varshini-student <mvarshini1708@gmail.com>
2026-05-20 11:23:59 +0530

    print the closeset integer

---
+++ Data_Handling/program12.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class program12{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        float num=scan.nextFloat();
+        System.out.printf("%d",Math.round(num));
+    }
+}
