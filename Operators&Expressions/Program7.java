b04e410d8be2ee77a5a02ba5015c5b9bce3f1382
varshini-student <mvarshini1708@gmail.com>
2026-05-24 10:21:42 +0530

    two inputs one output

---
+++ Operators&Expressions/Program7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        float L=scan.nextFloat();
+        float W=scan.nextFloat();
+        float area=L*W;
+        float yards=area/9;
+        System.out.printf("%.2f",yards);
+        
+    }
+}
