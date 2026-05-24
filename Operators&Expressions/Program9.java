bae296d72ad25925f9b6129bef886e506c52ec6b
varshini-student <mvarshini1708@gmail.com>
2026-05-24 10:26:13 +0530

    Added a celsius program

---
+++ Operators&Expressions/Program9.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        float f=scan.nextFloat();
+        float c=5.0f/9.0f*(f-32);
+        System.out.printf("%.2f",c);
+    }
+}
