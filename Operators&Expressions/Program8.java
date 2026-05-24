f04116dcee3213b8d3d2210d5f3168fbdfdf0973
varshini-student <mvarshini1708@gmail.com>
2026-05-24 10:23:55 +0530

    Years calculation

---
+++ Operators&Expressions/Program8.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+       Scanner scan =new Scanner(System.in);
+       int age=scan.nextInt();
+       int years=7*age;
+       System.out.printf("%d years old in dog years.",years);
+       
+    }
+}
