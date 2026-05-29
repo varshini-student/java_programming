4e5a079c58532e19d7943bba821437eb06c9c7ff
varshini-student <mvarshini1708@gmail.com>
2026-05-29 20:06:13 +0530

    Area of perimeter

---
+++ Operators&Expressions/Program16.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int l=scan.nextInt();
+        int b=scan.nextInt();
+        int area=l*b;
+        int perimeter=2*(l+b);
+        System.out.printf("Area: %d\n",area);
+        System.out.printf("Perimeter: %d",perimeter);
+    }
+}
