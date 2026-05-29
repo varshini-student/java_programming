39797e7f3de8c5594c1c4056c44b9e92ac10b311
varshini-student <mvarshini1708@gmail.com>
2026-05-29 19:51:42 +0530

    Added a 6 precision points

---
+++ Operators&Expressions/Program15.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+     Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int result =(num+8)/3;
+        // result= result/3.0;
+        result= result%5;
+        result= result*5;
+        
+        System.out.printf("%.6f",(double)result);
+    }
+}
