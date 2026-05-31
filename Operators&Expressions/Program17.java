9e60fd39cd5f1b96b00ede72f4ac31fe321f529e
varshini-student <mvarshini1708@gmail.com>
2026-05-31 19:56:47 +0530

    Yeras calculation sum

---
+++ Operators&Expressions/Program17.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int days = scan.nextInt();
+        int years = days / 365;
+        int rdays = days % 365;
+        int weeks = rdays / 7;
+        int remDays = rdays % 7;
+        System.out.println("Years: " + years);
+        System.out.println("Weeks: " + weeks);
+        System.out.println("Days: " + remDays);
+
+    }
+
+}
