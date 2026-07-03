9558c8d55a489abbaa3cc9db08899386e5a86ae5
varshini-student <mvarshini1708@gmail.com>
2026-07-03 09:46:02 +0530

    Added sort colors program

---
+++ Arrays/Program3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int start = 0;
+        int end = nums.length - 1;
+        int index = 0;
+
+        while (index <= end) {
+
+            if (nums[index] == 0) {
+                int temp = nums[start];
+                nums[start] = nums[index];
+                nums[index] = temp;
+
+                start++;
+                index++;
+            }
+
+            else if (nums[index] == 2) {
+                int temp = nums[end];
+                nums[end] = nums[index];
+                nums[index] = temp;
+
+                end--;
+            }
+
+            else {
+                index++;
+            }
+        }
+    }
+}
