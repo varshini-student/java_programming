0f40a3226bbc247da87a191af7ea18f82d8c4334
varshini-student <mvarshini1708@gmail.com>
2026-07-04 10:08:27 +0530

    Added Non overlapping Intervals

---
+++ Arrays/Program10.java
@@ -0,0 +1,21 @@
+// Non overlapping Intervals
+import java.util.*;
+public class Program10 {
+    
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a,b) -> a[1] - b[1]);
+        int count =0;
+        int cEnd = intervals[0][1];
+        for(int i=1; i<intervals.length; i++){
+            if(intervals[i][0] < cEnd){
+                count++;
+            }
+            else{
+                cEnd = intervals[i][1];
+            }
+
+        }
+        return count;
+    }
+}
+
